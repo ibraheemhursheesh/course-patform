1b5821ad6afe1bd261588366646a0bcec05eae3d
ibrahimharchiche <ibrahimharchiche44@gmail.com>
2025-12-31 09:39:27 +0100

    video poster bug fix

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -76,7 +76,7 @@ export default function VideoPlayer(props) {
         onEnded={(e) => addWatchedLesson(lesson)}
         data-plyr-config='{"previewThumbnails": { "enabled": false, "src": "", "withCredentials": false },"keyboard": { "focused": true, "global": true },"controls": ["play-large", "play", "rewind", "fast-forward", "progress", "current-time", "duration", "mute", "volume", "captions", "settings", "fullscreen"], "speed": { "selected": 1, "options": [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 4] }}'
         ref={setRef}
-        data-poster="./wel.png"
+        dataPoster="./wel.png"
         controls={controls}
         crossOrigin={crossOrigin}
         playsInline={playsInline}
